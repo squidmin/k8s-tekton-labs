250eba94b26ef34b4639db708ee9621a8200250e
squidmin <morse.james.r@gmail.com>
2023-09-02 21:08:59 -0500

    edit: proofreading

--- scripts/kubectl/apply.sh
+++ scripts/kubectl/apply.sh
@@ -4,6 +4,7 @@
 
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.9/git-clone.yaml
+#kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.4/git-clone.yaml
 kubectl apply -f https://api.hub.tekton.dev/v1/resource/tekton/task/kaniko/0.6/raw
 
 kubectl apply -f crds/PersistentVolumeClaims/
